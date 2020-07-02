d6e0e9cff01ab337ee6c60282fb20034f859a7d9
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2020-07-02 23:13:03 +0400

    Task 34 finished

--- Euler 31-40/task34.c
+++ Euler 31-40/task34.c
@@ -8,8 +8,7 @@ long int fact(int num);
 int main()
 {
     long int sum = 0;
-    printf("%ld\n\n", fact(9));
-    for (long int i = 10; i < 9999999; i++)
+    for (long int i = 10; i < 2540160; i++)
         if (checkSum(i))
             sum += i;
     printf("%ld\n", sum);
@@ -20,10 +19,11 @@ int main()
 int checkSum(long int num)
 {
     long int sum = 0;
-    while (num > 0)
+    long int temp = num;
+    while (temp > 0)
     {
-        sum += fact(num % 10);
-        num /= 10;
+        sum += fact(temp % 10);
+        temp /= 10;
     }
     if (num == sum)
         return 1;
@@ -33,7 +33,7 @@ int checkSum(long int num)
 
 long int fact(int num)
 {
-    if (num == 1)
+    if (num == 0)
         return 1;
     return num * fact(num - 1);
 }
