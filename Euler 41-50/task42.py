2712cf31b4c109655ea58e7f9efb0ceb7c3ddb5a
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2020-07-03 18:37:43 +0400

    New file

---
+++ Euler 41-50/task42.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+def word_value(word):
+    value = 0
+    for i in word:
+        value += ord(i) - 64
+    return value
+
+def is_triangle(word):
+    value = word_value(word)
+    temp = discriminant(value * 2)
+    if temp == int(temp):
+        return True
+    return False
+
+def discriminant(num):
+    d = 1 + 4 * num
+    return (-1 + sqrt(d)) / 2
+
+
+#importing words
+f = open("D:\\Programming\\ProjectEuler\\Euler 41-50\\words.txt", "r")
+words = [i[1:-1] for i in f.read().split(",")]
+f.close()
+
+
+count = 0
+for i in words:
+    if is_triangle(i):
+        count += 1
+
+print(count)
