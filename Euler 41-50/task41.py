e0900f6353877f5bac9635fe4551f59c74825998
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2020-07-03 18:13:05 +0400

    Rapid version

--- Euler 41-50/task41.py
+++ Euler 41-50/task41.py
@@ -1,36 +1,23 @@
-def primes_list(n):
-    lst=[2]
-    for i in range(3, n+1, 2):
-        if (i > 10) and (i%10==5):
-            continue
-        for j in lst:
-            if j*j-1 > i:
-                print("Prime:", i)
-                lst.append(i)
-                break
-            if (i % j == 0):
-                break
-        else:
-            print("Prime:", i)
-            lst.append(i)
-    return lst
+from itertools import permutations
 
-def is_prime(num, lst):
-    if num in lst:
-        return True
-    return False
 
-def pandigital(num):
-    if len(set(str(num))) == num and '0' not in str(num):
-        return True
-    return False
+def is_prime(num):
+    if num % 2 == 0:
+        return False
+    for i in range(3, num, 2):
+        if num % i == 0:
+            return False
+    return True
 
-print("Primes are not ready")
-primes = primes_list(987654321)
-print("Primes are ready")
 
-for i in range(987654321, 2, -1):
+numbers = []
+for i in range(9, 0, -1):
+    numbers.extend(list(map(int, [''.join(c) for c in permutations("".join([str(x) for x in range(1, i + 1)]))])))
+numbers.sort()
+
+
+for i in numbers[::-1]:
     print(i)
-    if pandigital(i) and is_prime(i, primes):
-        print("The answer is:", i)
+    if is_prime(i):
+        print("Answer", i)
         break
