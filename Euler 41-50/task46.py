5602882d94eaf5eb11364b35c81d8f0958d29d5d
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2020-07-04 18:07:20 +0400

    Task46 is added

---
+++ Euler 41-50/task46.py
@@ -0,0 +1,38 @@
+from math import sqrt
+
+
+def is_prime(num, lst):
+    for i in lst:
+        if num % i == 0:
+            return False
+    return True
+
+
+def fill_primes(num, lst):
+    for i in range(lst[-1], num):
+        if is_prime(i, lst):
+            lst.append(i)
+    return lst
+
+
+def is_sum(num, lst):
+    for i in lst:
+        temp = sqrt( (num - i) / 2 )
+        if temp == int(temp):
+            return True
+    return False
+
+
+def find_answer():
+    primes = [2, 3]
+    odd = 1
+    while True:
+        odd += 2
+        primes = fill_primes(odd, primes)
+        if is_prime(odd, primes):
+            continue
+        if not is_sum(odd, primes):
+            return odd
+
+result = find_answer()
+print(result)
