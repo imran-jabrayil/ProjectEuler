2b47fee4e989d1f71502b14584dd213b8e4a3f2d
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2020-07-05 23:33:54 +0400

    New file

---
+++ Euler 61-70/task67.py
@@ -0,0 +1,30 @@
+from time import time
+
+
+def calculate(triang):
+    if len(triang) == 1:
+        return triang[0][0]
+    
+    last_line = triang.pop(-1)
+
+    for i in range(len(last_line) - 1):
+        if last_line[i] > last_line[i + 1]:
+            triang[-1][i] += last_line[i]
+        else:
+            triang[-1][i] += last_line[i + 1]
+    
+    return calculate(triang)
+    
+
+
+
+
+
+start = time()
+
+f = open("D:\\Programming\\ProjectEuler\\Euler 61-70\\triangle.txt", "r")
+nums = [list(map(int, i.split())) for i in f.readlines()]
+
+print(calculate(nums))
+
+print("Time {} s.".format(time() - start))
