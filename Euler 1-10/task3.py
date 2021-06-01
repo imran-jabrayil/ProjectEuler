6851e59d872ba8efd357c3b8839a278e2ffb26bc
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2021-06-02 01:52:37 +0400

    New solution

--- Euler 1-10/task3.py
+++ Euler 1-10/task3.py
@@ -1,34 +1,18 @@
 from time import time
-from math import sqrt
-
-
-def divs(n):
-    x = 2
-    divisors = set()
-    while n != 1:
-        if n % x == 0:
-            n //= x
-            divisors.add(x)
-        else:
-            x += 1
-            if x > sqrt(n):
-                divisors.add(n)
-                break
-    return sorted(list(divisors))
-
-
-def is_prime(num):
-    return pow(2, num - 1, num) == 1
-
-
-def max_prime_div(num):
-    d = divs(n)
-    for i in d[::-1]:
-        if is_prime(i):
-            return i
-
 
 start = time()
+
+def max_prime_div(number):
+    for i in range(2, number // 2):
+        if number == i:
+            break
+        while not number % i:
+            number /= i
+
+    return number
+        
+
 n = 600851475143 
 print(max_prime_div(n))
+
 print("Time {} s.".format(time() - start))
