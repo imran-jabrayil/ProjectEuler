2757ffc49ebb921846ce7024a2f089b53e02b8a1
Imran Jabrayilov <imran.jabrayilov@etu.unistra.fr>
2021-09-01 19:47:03 +0400

    Little changes

--- Euler 71-80/task79.py
+++ Euler 71-80/task79.py
@@ -1,32 +1,55 @@
 from itertools import combinations
+from time import time
+from typing import List
 
 
-def import_nums(fn):
+def import_nums(fn: str) -> List[str]:
     with open(fn, 'r') as f:
-        nums = [i[:-1] for i in f.readlines()]
-    return nums
+        nums = set(i[:-1] for i in f.readlines())
+    return list(nums)
 
 
-def after(nums, x):
-    result = ''
-    for i in nums:
-        if x in i:
-            result += i[i.index(x) + 1:]
-    return len(set(result))
+def number_in_result(number: str, result: str) -> bool:
+    for i in range(len(number)):
+        if number[i] in result:
+            result = result[result.index(number[i]) + 1:]
+        else:
+            return False
+
+    return True
 
 
-def calculate():
-    nums = import_nums('p079_keylog.txt')
-    elems = set(''.join(nums))
-    result = ''
-    while elems:
-        for i in elems:
-            temp = after(nums, i)
-            if temp == len(elems) - 1:
-                result += i
-                elems.discard(i)
-                break
+def calculate() -> str:
+    nums: List[str] = import_nums('Euler 71-80//p079_keylog.txt')
+    result: str = nums.pop(0)
+
+    while len(nums) != 0:
+        index: int = 0
+
+        while index < len(nums):
+            temp_number = nums[index]
+
+            if number_in_result(temp_number, result):
+                nums.pop(index)
+                continue
+
+            if temp_number[1] == result[-1]:
+                result += temp_number[2]
+                index += 1
+                continue
+
+            if temp_number[0] in result and temp_number[2] in result \
+                and (index_start := result.index(temp_number[0])) + 1 == (index_end := result.index(temp_number[2])):
+                result = result[:index_start + 1] + temp_number[1] + result[index_end:]
+                nums.pop(index)
+                continue
+
+            index += 1
+
     return result
 
 
-print(calculate())
+if __name__ == "__main__":
+    start = time()
+    print(calculate())
+    print(f"Time: {time() - start}")
